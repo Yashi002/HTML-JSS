1310d3bb1e3d703f625a76a9a23e6c9bda67f601
Yashi002 <50695892+Yashi002@users.noreply.github.com>
2021-05-25 11:18:19 +0530

    Create age.js

---
+++ age.js
@@ -0,0 +1,18 @@
+
+//Using comparison operators to print the result 
+
+const age = 24;
+console.log (age >= 18);
+// Output the result of age < 18
+console.log (age < 18);
+// When the value of age is 18 or greater, output "I am 18 or older"
+if (age >= 18) {
+  console.log("I am 18 or older");
+}
+
+const age = 24;
+
+// Adding an if statement with the specified conditions
+if (age >= 20 && age < 30 ) {
+  console.log("I am in my 20s");
+}
