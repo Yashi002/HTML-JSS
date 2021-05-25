ea3472b40d12e587ff8275f93823828f2ba4e7f5
Yashi002 <50695892+Yashi002@users.noreply.github.com>
2021-05-25 11:15:05 +0530

    Create Script1.js

---
+++ Script1.js
@@ -0,0 +1,13 @@
+// Output combination of "Yashi " and "Verma"
+console.log("Yashi " + "Verma");
+
+
+console.log("20" + "15");
+
+//Declare the variable length
+ //to store the value of the radius and assign the value 5 to it.
+
+
+let length= 5;
+console.log(length);
+console.log(length*length*3);
