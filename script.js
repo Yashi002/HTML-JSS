586711a937a297084e18f3407e7175c16e06b516
Yashi002 <50695892+Yashi002@users.noreply.github.com>
2021-05-25 11:11:19 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Welcome to Study I!");
+console.log("Let's learn with Ken the Robot!");
+
+console.log("Hello World");
+
+console.log("Ken the Robot");
+
+
+// Output the result of 5 plc
+console.log(5 + 3);
+// Output the result of 20 minus 8
+console.log(20 - 8);
+
+// Output the string "4 + 5"
+console.log("4+5");
